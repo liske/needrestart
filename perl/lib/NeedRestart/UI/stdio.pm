c6c386ac2b5dc4316314f15848e4328305e674fe
Thomas Liske <thomas@fiasko-nw.net>
2014-05-10 20:00:52 +0200

    Read explicitly from STDIN (fixes weird error messages).

--- perl/lib/NeedRestart/UI/stdio.pm
+++ perl/lib/NeedRestart/UI/stdio.pm
@@ -65,7 +65,7 @@ sub announce {
     my $kmessage = shift;
 
     print "Pending kernel upgrade!\n\nRunning kernel version:\n  ${kversion}\n\nDiagnostics:\n  ${kmessage}\n\nYou should consider to reboot this machine to activate the pending kernel upgrade. You need to reboot MANUALLY! [Return]\n";
-    <>;
+    <STDIN>;
 }
 
 
