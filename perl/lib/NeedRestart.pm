4643c4ecf59a2d003b4685620dd63673cd74eb47
Thomas Liske <thomas@fiasko-nw.net>
2016-01-05 22:26:34 +0100

    Update version to 2.5.

--- perl/lib/NeedRestart.pm
+++ perl/lib/NeedRestart.pm
@@ -4,7 +4,7 @@
 #   Thomas Liske <thomas@fiasko-nw.net>
 #
 # Copyright Holder:
-#   2013 - 2015 (C) Thomas Liske [http://fiasko-nw.net/~thomas/]
+#   2013 - 2016 (C) Thomas Liske [http://fiasko-nw.net/~thomas/]
 #
 # License:
 #   This program is free software; you can redistribute it and/or modify
@@ -77,7 +77,7 @@ our %EXPORT_TAGS = (
     )],
 );
 
-our $VERSION = '2.4';
+our $VERSION = '2.5';
 my $LOGPREF = '[Core]';
 
 my %UIs;
