liske/needrestart

commit f8c2609f8d5a0e10bd988497b8ea9815a7bb2fa8

[uCode] Modernize and fix error handling.

perl/lib/NeedRestart/uCode.pm
@@ -139,13 +139,16 @@ sub nr_ucode_check {
 
         # call ucode modules
         foreach my $pkg (@PKGS) {
-            my ( $processor, @nvars) = ('?');
-            eval "(\$processor, \@nvars) = ${pkg}::nr_ucode_check_real(\$debug, \$ui, \$processors{\$pid});";
-            print STDERR $@
-              if ( $@ && $debug );
+            my @nvars;
+            eval "\@nvars = ${pkg}::nr_ucode_check_real(\$debug, \$ui, \$processors{\$pid});";
+            if ( $@ && $debug ) {
+                print STDERR $@;
+                $ui->progress_step;
+                next;
+            }
             $ui->progress_step;
 
-	    my $nstate = compare_ucode_versions( $debug, $processor, @nvars );
+            my $nstate = compare_ucode_versions( $debug, $processors{processor}, @nvars );
             if ( $nstate > $state ) {
                 ( $state, @vars ) = ( $nstate, @nvars );
             }

perl/lib/NeedRestart/uCode/AMD.pm
@@ -190,7 +190,7 @@ sub nr_ucode_check_real {
 	}
     }
 
-    return ( $info->{processor}, %vars );
+    return %vars;
 }
 
 1;

perl/lib/NeedRestart/uCode/Intel.pm
@@ -64,7 +64,7 @@ sub nr_ucode_check_real {
 "$LOGPREF #$info->{processor} current microcode revision not found in /proc/cpuinfo: $!\n"
           if ($debug);
 
-        return ( NRM_UNKNOWN, %vars );
+        return %vars;
     }
 
     # find and cache microcode updates
@@ -83,7 +83,7 @@ sub nr_ucode_check_real {
     }
     $vars{AVAIL} = $_avail if ( defined($_avail) );
 
-    return ( $info->{processor}, %vars );
+    return %vars;
 }
 
 1;

commit e85bfe33b595b88cc8052a7815d13612ecc2a841

[uCode] fix uninitialized value in logging of processor index
This got broken in f8c2609f8d5a0e10bd988497b8ea9815a7bb2fa8.

Before that it would have effectively logged
`$processors{$pid}->{processor}`, but the `processor` entry
is also the key in `%processors`, i.e. equals `$pid`.

perl/lib/NeedRestart/uCode.pm
@@ -148,7 +148,7 @@ sub nr_ucode_check {
             }
             $ui->progress_step;
 
-            my $nstate = compare_ucode_versions( $debug, $processors{processor}, @nvars );
+            my $nstate = compare_ucode_versions( $debug, $pid, @nvars );
             if ( $nstate > $state ) {
                 ( $state, @vars ) = ( $nstate, @nvars );
             }