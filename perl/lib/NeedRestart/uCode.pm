liske/needrestart

commit f8c2609f8d5a0e10bd988497b8ea9815a7bb2fa8

[uCode] Modernize and fix error handling.

perl/lib/NeedRestart/uCode.pm
@@ -139,13 +139,16 @@ sub nr_ucode_check {
 
         # call ucode modules
         foreach my $pkg (@PKGS) {
-            my ( $processor, @nvars) = ('?');
-            eval "(\$processor, \@nvars) = ${pkg}::nr_ucode_check_real(\$debug, \$ui, \$processors{\$pid});";
-            print STDERR $@
-              if ( $@ && $debug );
+            my @nvars;
+            eval "\@nvars = ${pkg}::nr_ucode_check_real(\$debug, \$ui, \$processors{\$pid});";
+            if ( $@ && $debug ) {
+                print STDERR $@;
+                $ui->progress_step;
+                next;
+            }
             $ui->progress_step;
 
-	    my $nstate = compare_ucode_versions( $debug, $processor, @nvars );
+            my $nstate = compare_ucode_versions( $debug, $processors{processor}, @nvars );
             if ( $nstate > $state ) {
                 ( $state, @vars ) = ( $nstate, @nvars );
             }

perl/lib/NeedRestart/uCode/AMD.pm
@@ -190,7 +190,7 @@ sub nr_ucode_check_real {
 	}
     }
 
-    return ( $info->{processor}, %vars );
+    return %vars;
 }
 
 1;

perl/lib/NeedRestart/uCode/Intel.pm
@@ -64,7 +64,7 @@ sub nr_ucode_check_real {
 "$LOGPREF #$info->{processor} current microcode revision not found in /proc/cpuinfo: $!\n"
           if ($debug);
 
-        return ( NRM_UNKNOWN, %vars );
+        return %vars;
     }
 
     # find and cache microcode updates
@@ -83,7 +83,7 @@ sub nr_ucode_check_real {
     }
     $vars{AVAIL} = $_avail if ( defined($_avail) );
 
-    return ( $info->{processor}, %vars );
+    return %vars;
 }
 
 1;

commit 62f50f354f0c0f0265630231dce34b80e48cd9cc

[uCode] fix error handling logic being dependent on debug level, leading to "Use of uninitialized value $ucode_vars..." for non-verbose batch mode (-b) on Intel CPUs
The bug was introduced in f8c2609f8d5a0e10bd988497b8ea9815a7bb2fa8 (see https://github.com/liske/needrestart/commit/f8c2609f8d5a0e10bd988497b8ea9815a7bb2fa8#diff-22b45c8ce7df32d7f5f4fd1f5bbd262f42e168b2f6dcc6972b21d9a3681c7599R147)

perl/lib/NeedRestart/uCode.pm
@@ -154,8 +154,9 @@ sub nr_ucode_check {
         foreach my $pkg (@PKGS) {
             my @nvars;
             eval "\@nvars = ${pkg}::nr_ucode_check_real(\$debug, \$ui, \$processors{\$pid});";
-            if ( $@ && $debug ) {
-                print STDERR $@;
+            if ( $@ ) {
+                print STDERR $@
+                    if ($debug);
                 $ui->progress_step;
                 next;
             }